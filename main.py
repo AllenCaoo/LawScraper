adff0b6eb17ed49076070bdf264dd0eb06306612
Allen Cao <allencao@berkeley.edu>
2021-06-28 12:09:08 -0700

    Renamed function to re_init

--- main.py
+++ main.py
@@ -8,13 +8,16 @@ from bs4 import BeautifulSoup
 link = "https://www.congress.gov/search?pageSort=latestAction%3Adesc&q=%7B%22source%22%3A%" \
            "22legislation%22%2C%22bill-status%22%3A%22law%22%7D"
 def main():
-    init()
+    re_init()
 
-def init():
-    have_init = input("Want to re-initialize? y/n ")
-    if have_init == "n":
+def re_init(ask=False):
+    if ask:
+        want_init = input("Want to re-initialize? y/n ")
+    else:
+        want_init = "y"
+    if want_init == "n":
         return
-    elif have_init == "y":
+    elif want_init == "y":
         r = requests.get(link)
         soup = BeautifulSoup(r.content, "html.parser")
         recent = soup.find(class_="result-title bottom-padding").text
