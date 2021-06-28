4c37f547ffd1c84efd34057d57edb311049ec17f
Allen Cao <allencao@berkeley.edu>
2021-06-28 11:30:23 -0700

    Successfully scrapped sponsors and law_blocks

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ link = "https://www.congress.gov/search?pageSort=latestAction%3Adesc&q=%7B%22sou
 
 r = requests.get(link)
 soup = BeautifulSoup(r.content, "html.parser")
-titles = soup.find_all(class_="result-title bottom-padding")
-for tag in titles:
-    print(tag.text)
+law_blocks = soup.find_all(class_="expanded")
+for tag in law_blocks:
+    title = tag.find(class_="result-title")
+    sponsor = tag.find(class_="result-item").find("a")
+    print(f"{title.text}, sponsored by {sponsor.text}")
 
 
