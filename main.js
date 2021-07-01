a403305e5fe9788b7c069a9cae8dc6cd1bb48acd
Allen Cao <allencao@berkeley.edu>
2021-07-01 15:27:42 -0700

    Added email validation and created package.JSON

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
+//import check from 'email-existence';
+
 function formdata() {
-    let input = document.getElementById("firstname").value.toLowerCase().replace(/\s/g, "");
-    alert(input);
-    let input = document.getElementById("lastname").value.toLowerCase().replace(/\s/g, "");
-    alert(input);
-    let input = document.getElementById("email").value.toLowerCase().replace(/\s/g, "");
-    alert(input);
+    var first = document.getElementById("firstname").value.toLowerCase().replace(/\s/g, "");
+    var last = document.getElementById("lastname").value.toLowerCase().replace(/\s/g, "");
+    var email = document.getElementById("email").value.toLowerCase().replace(/\s/g, "");
+    //let exists = check(email, function(err, res) {
+    //    console.log('res: '+res);
+    //});
+    if (!validateEmail(email)) {
+        alert("invalid email")
+    }
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function saveInfo() {
+    
 }
