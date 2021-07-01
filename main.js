09798db162d64f1e19798cd2d06e0d2c20304bf2
Allen Cao <allencao@berkeley.edu>
2021-07-01 12:46:08 -0700

    Centered submission body

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 function formdata() {
+    let input = document.getElementById("firstname").value.toLowerCase().replace(/\s/g, "");
+    alert(input);
+    let input = document.getElementById("lastname").value.toLowerCase().replace(/\s/g, "");
+    alert(input);
     let input = document.getElementById("email").value.toLowerCase().replace(/\s/g, "");
     alert(input);
 }
