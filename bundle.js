23e2fe84f4aec5d58280a40d2996cc13bb51a5ba
Allen Cao <allencao@berkeley.edu>
2021-07-06 12:12:37 -0700

    Updated bundle

--- bundle.js
+++ bundle.js
@@ -1,6 +1,7 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.main = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 
 },{}],2:[function(require,module,exports){
+
 var fs = require('fs');
 
 function formdata() {
@@ -23,8 +24,7 @@ function validateEmail(email) {
 
 
 function saveInfo(name, email) {
-    let fileName = "backend/.info/subs.JSON";
-    var data = fs.readFileSync(fileName); // TODO: Error right here and needs to be fixed
+    var data = fs.readFileSync("backend/.info/subs.JSON", 'utf8'); // TODO: Error right here and needs to be fixed
     var dict = JSON.parse(data);
     let entry = {
         name: email
@@ -41,5 +41,4 @@ window.formdata = formdata;
 window.validateEmail = validateEmail;
 window.saveInfo = saveInfo;
 
-},{"fs":1}]},{},[2])(2)
-});
+},{"fs":1}]},{},[2]);
