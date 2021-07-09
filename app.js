bf19223015ea192de885fa20e6ceb556b713d640
Allen Cao <allencao@berkeley.edu>
2021-07-09 12:52:20 -0700

    Successfully loading webpage with image

--- app.js
+++ app.js
@@ -1,33 +1,12 @@
-// imports 
-const http = require('http');
-const fs = require('fs');
 const express = require('express');
+const path = require('path');
+
 const app = express();
-const port = 3000;
-
-app.use(express.static(__dirname+'/public'));
-app.use('/images', express.static(__dirname + 'public/images'));
-app.use('/scripts', express.static(__dirname + 'public/scripts'));
 
 
-const server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': "text/html" });
-    fs.readFile('views/index.html', function(error, data) {
-        if (error) {
-            res.writeHead(404);
-            res.write('Error: File not found');
-        } else {
-            res.write(data);
-        }
-        res.end();
-    })
-})
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-// listen on port
-server.listen(port, function(error) {
-    if (error) {
-        console.log("errored");
-    } else {
-        console.log("listening...");
-    }
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
